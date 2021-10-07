7ad92358bf2551abe0bee86fa866747741550855
harishcalvin <hari991996@gmail.com>
2021-10-07 16:41:33 +0530

    add hamburger

--- file.js
+++ file.js
@@ -1,1 +1,9 @@
-// todo
+const burger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.nav--menu');
+burger.addEventListener('click', burgertoggle);
+
+function burgertoggle() {
+    burger.classList.toggle('toggle');
+    navigation.classList.toggle('show--nav--menu');
+
+}
